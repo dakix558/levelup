86c0b09766297c2f53cf5f943f9f5c0414325971
dakix558 <mrdaca066@gmail.com>
2023-02-06 19:43:46 +0100

    my first commit

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+    function enqueue_style_script() {
+        $ver = '1.0.0';
+        
+        wp_register_style('maincss', get_template_directory_uri() . '/assets/css/main.css', array(), $ver, 'all');
+        wp_enqueue_style('maincss');
+    
+
+        wp_register_script('mainjs', get_template_directory_uri() . '/assets/js/main.js', array(), $ver, 1, 1);
+        wp_enqueue_script('mainjs');
+
+    }
+    add_action('wp_enqueue_scripts', 'enqueue_style_script');
+
+?>
